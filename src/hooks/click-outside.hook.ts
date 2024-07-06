b116e980f9249182099e9eb175b267443450e6c0
Coding Scale <codingscale157@gmail.com>
2024-07-06 21:06:26 +0530

    POR-6:feat:added prettier to all files

--- src/hooks/click-outside.hook.ts
+++ src/hooks/click-outside.hook.ts
@@ -1,24 +1,24 @@
 import { RefObject, useEffect } from 'react';
 
 type TOptions = {
-    ref: RefObject<HTMLElement>;
-    callback: () => void;
+  ref: RefObject<HTMLElement>;
+  callback: () => void;
 };
 
 function useClickOutside({ ref, callback }: TOptions) {
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (ref.current && !ref.current.contains(event.target as Node)) {
-                callback();
-            }
-        };
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
 
-        document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
 
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [callback, ref]);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [callback, ref]);
 }
 
 export default useClickOutside;
